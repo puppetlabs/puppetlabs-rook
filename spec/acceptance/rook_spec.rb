62b33445825c3b95f582d061bccfc3e923ed8ab9
sheena <sheena@puppet.com>
2020-01-02 16:07:29 +0000

    (FM-8205) port module to litmus

--- spec/acceptance/rook_spec.rb
+++ spec/acceptance/rook_spec.rb
@@ -27,100 +27,79 @@ describe 'the rook module' do
         }
   MANIFEST
       it 'should run' do
-        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp)
       end
       it 'should install kubectl' do
-        shell('kubectl', :acceptable_exit_codes => [0])
+        run_shell('kubectl')
       end
       it 'should install kube-dns' do
-        shell('export KUBECONFIG=/etc/kubernetes/admin.conf; kubectl get deploy --namespace kube-system coredns', :acceptable_exit_codes => [0])
+        run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf; kubectl get deploy --namespace kube-system coredns')
         sleep(60)
       end
     end
   end
+#   describe 'rook class' do
+#     context 'it should install the module' do
+#       let(:pp) {"
+#       include rook
+#       "}
+#       it 'should run' do
+#         apply_manifest(pp, :catch_failures => true)
+#       end
 
-  # describe 'rook class' do
-  #   context 'it should install the module' do
-  #     let(:pp) {"
-  #     include rook
-  #     "}
-  #     it 'should run' do
-  #       apply_manifest(pp, :catch_failures => true)
-  #     end
+#       it 'should create rook namespace' do
+#         run_shell('sleep 120')
+#         run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl get namespaces | grep rook')
+#       end
 
-  #     if fact('osfamily') == 'Debian'
-  #       [ 'ceph-common',
-  #         'ceph-fs-common'
-  #       ].each do | package_name |
-  #         it 'should contain packages' do
-  #           expect(package(package_name)).to be_installed
-  #         end
-  #       end
-  #     end
+#       it 'should create rook operator ' do
+#         run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook-system get pod | grep rook-operator') do |r|
+#             expect(r.stdout).to match(/Running/)
+#         end
+#       end
 
-  #     if fact('osfamily') == 'RedHat'
-  #       [ 'ceph-common',
-  #         'ceph-deploy'
-  #       ].each do | package_name |
-  #         it 'should contain packages' do
-  #           expect(package(package_name)).to be_installed
-  #         end
-  #       end
-  #     end
+#       it 'should create rook agent ' do
+#         run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook-system get pod | grep rook-agent') do |r|
+#             expect(r.stdout).to match(/Running/)
+#         end
+#       end
 
-  #     it 'should create rook namespace' do
-  #       shell('sleep 120')
-  #       shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl get namespaces | grep rook', :acceptable_exit_codes => [0])
-  #     end
-
-  #     it 'should create rook operator ' do
-  #       shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook-system get pod | grep rook-operator', :acceptable_exit_codes => [0]) do |r|
-  #           expect(r.stdout).to match(/Running/)
-  #       end
-  #     end
-
-  #     it 'should create rook agent ' do
-  #       shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook-system get pod | grep rook-agent', :acceptable_exit_codes => [0]) do |r|
-  #           expect(r.stdout).to match(/Running/)
-  #       end
-  #     end
-
-  #     it 'should create rook cluster and verify rook-api' do
-  #       shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-api', :acceptable_exit_codes => [0]) do |r|
-  #           expect(r.stdout).to match(/Running/)
-  #       end
-  #     end
-  #     it 'should create rook cluster and verify rook-ceph-mgr0' do
-  #       shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-ceph-mgr0', :acceptable_exit_codes => [0]) do |r|
-  #           expect(r.stdout).to match(/Running/)
-  #       end
-  #     end
-  #     it 'should create rook cluster and verify rook-ceph-mon0' do
-  #       shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-ceph-mon0', :acceptable_exit_codes => [0]) do |r|
-  #           expect(r.stdout).to match(/Running/)
-  #       end
-  #     end
-  #     it 'should create rook cluster and verify rook-ceph-mon1' do
-  #       shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-ceph-mon1', :acceptable_exit_codes => [0]) do |r|
-  #           expect(r.stdout).to match(/Running/)
-  #       end
-  #     end
-  #     it 'should create rook cluster and verify rook-ceph-mon2' do
-  #       shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-ceph-mon2', :acceptable_exit_codes => [0]) do |r|
-  #           expect(r.stdout).to match(/Running/)
-  #       end
-  #     end
-  #     it 'should create rook cluster and verify rook-ceph-osd' do
-  #       shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-ceph-osd', :acceptable_exit_codes => [0]) do |r|
-  #           expect(r.stdout).to match(/Running/)
-  #       end
-  #     end
-  #     it 'should create storage class' do
-  #       shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl get storageclass | grep rook-block', :acceptable_exit_codes => [0])
-  #     end
-  #     # it 'should set default storage class' do
-  #     #   shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubeget get storageclass | grep "rook-block (default)"',:acceptable_exit_codes => [0])
-  #     # end
-  #   end
-  # end
+#       it 'should create rook cluster and verify rook-api' do
+#         run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-api') do |r|
+#             expect(r.stdout).to match(/Running/)
+#         end
+#       end
+#       it 'should create rook cluster and verify rook-ceph-mgr0' do
+#         shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-ceph-mgr0') do |r|
+#             expect(r.stdout).to match(/Running/)
+#         end
+#       end
+#       it 'should create rook cluster and verify rook-ceph-mon0' do
+#         run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-ceph-mon0') do |r|
+#             expect(r.stdout).to match(/Running/)
+#         end
+#       end
+#       it 'should create rook cluster and verify rook-ceph-mon1' do
+#         run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-ceph-mon1') do |r|
+#             expect(r.stdout).to match(/Running/)
+#         end
+#       end
+#       it 'should create rook cluster and verify rook-ceph-mon2' do
+#         run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-ceph-mon2') do |r|
+#             expect(r.stdout).to match(/Running/)
+#         end
+#       end
+#       it 'should create rook cluster and verify rook-ceph-osd' do
+#         run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl -n rook get pod | grep rook-ceph-osd') do |r|
+#             expect(r.stdout).to match(/Running/)
+#         end
+#       end
+#       it 'should create storage class' do
+#         run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubectl get storageclass | grep rook-block')
+#       end
+#       it 'should set default storage class' do
+#         run_shell('export KUBECONFIG=/etc/kubernetes/admin.conf;kubeget get storageclass | grep "rook-block (default)"')
+#       end
+#     end
+#   end
 end
