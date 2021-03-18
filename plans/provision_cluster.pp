365765aa4a384bc5e9a837105a1ca46b8ac9b3ea
sheenaajay <sheena@puppet.com>
2021-03-18 09:47:59 +0000

    add win box

--- plans/provision_cluster.pp
+++ plans/provision_cluster.pp
@@ -14,4 +14,6 @@ plan rook::provision_cluster(
     action => 'provision', platform => $gcp_image, vars => 'role: worker1')
   run_task('provision::provision_service', 'localhost',
     action => 'provision', platform => $gcp_image, vars => 'role: worker2')
+  run_task('provision::provision_service', 'localhost',
+    action => 'provision', platform => 'windows-2012-r2-core', vars => 'role: test')
 }
